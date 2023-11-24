5cd388f9aedcb2571441599a48218a133dd59496
Igor Dimitrov <igor-dimi@protonmail.com>
2023-11-24 13:04:45 +0100

    added the program for iterative binomial

---
+++ 03/binit.cc
@@ -0,0 +1,28 @@
+#include "../code/fcpp.hh"
+
+//iterative Implementierung der Fakultaetsfunktion durch Tail-recursion
+int fakit(int res, int n)
+{
+    return cond(
+        n > 1,
+        fakit(res * n, n - 1),
+        res
+    );
+}
+
+int fak(int n)
+{
+    return fakit(1, n);
+}
+
+int binit(int n, int k)
+{
+    return fak(n) / (fak(k) * fak(n - k));
+}
+
+int main(int argc, char** argv)
+{
+    return print(binit(
+        readarg_int(argc, argv, 1),
+        readarg_int(argc, argv, 2)));
+}
